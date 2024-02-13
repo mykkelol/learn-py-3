482c3607f86ea71759a1b75046342826d81d8a84
mykkelol <1micheal.sihavong2@gmail.com>
2024-02-12 22:57:33 -0800

    add section 7 string and variable refresher

--- sect7_string_variable.py
+++ sect7_string_variable.py
@@ -1,5 +1,4 @@
-print('How many kilometers did Biden run today?')
-kms = input()
+kms = input('How many kilometers did Biden run today?')
 miles = float(kms) / 1.60934
 miles = round(miles, 2)
 print(f'Ok you said Biden ran {kms} kilometers and in USA he ran {miles} miles!')
