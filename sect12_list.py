434ab9b3139b8afc8a518d931f503df8aecbd90c
mykkelol <1micheal.sihavong2@gmail.com>
2024-02-17 23:46:39 -0800

    add section 12 relearn list

---
+++ sect12_list.py
@@ -0,0 +1,16 @@
+""" 
+
+Common list functions
+    - list[-1] - reverse retrieval starts at -1
+
+Adding to list
+    - .append()
+    - .extend()
+    - .insert() - insert at specific index but if with -1, never last index but rather index before last 
+
+Removing from list
+    - .clear() - clear list completely
+    - .pop() - remove last index if not specifed or specific index if specified
+    - .remove() - remove first value in list e.g [1,3,3,7].remove(3) will remove only index 1
+
+"""
