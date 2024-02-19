3c5a9baf70da4cc9aad1cf4a7737daf63abe50f7
mykkelol <1micheal.sihavong2@gmail.com>
2024-02-18 19:50:06 -0800

    finish sect16 tuples and sets

---
+++ sect15_tuples_and_sets.py
@@ -0,0 +1,35 @@
+""" 
+Tuples
+    - ordered collection or grouping of items that's immutable
+        - example: numbers = (1, 2, 3, 4)
+        - example: tuple([1,3,3,7]) = (1, 3, 3, 7)
+        - example: (1,3,(3,7))[2][1] = 7
+        - example: (1,) cannot be (1)
+    - given it's immutable, it's fast and more lightweight than lists
+    - tuples can be used as keys in dict
+        - example: { (37.7749, 122.4449) : 'San Francisco office' }
+
+Tuple methods
+    - .count(value)
+    - .index(value)
+
+Sets
+    - collection of items that don't have duplicated values and are not ordered
+        - example: set({1,2,3,4,5,5,5}) = {1,2,3,4,5}
+        - example: {1,3,3,7}
+    - cannot be accessed by index and is lightweight than lists or dict but can still be accessed via loop
+        - example: for num in {1,3,3,7}
+    - useful to retrieve uniques
+        - example: list(set([1,3,3,7])) = [1,3,7]
+
+Set methods
+    - .add(value)
+    - .remove(value) - will error if value doesn't exist
+    - .discard(value) - similar to remove but doesn't error
+    - .copy()
+    - .clear()
+    - .intersection()
+    - set1 | set2 - set union between two sets to create a set of unique values; similar to union all
+    - set1 & set2 - set intersection between two sets to create a set of common values; similar to inner join
+
+"""
